e4961e872ff16b6a2fe1dd53548d43600007932f
Alexandr Yakovlev <123alexher123@gmail.com>
2026-01-23 19:13:03 +0300

    поиск селенид в гитхаб готов

---
+++ src/test/java/github/BestContributor.java
@@ -0,0 +1,29 @@
+package github;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BestContributor {
+
+    @BeforeAll
+    static void setup() {
+        browserSize = "2560x1440";
+    }
+    @Test
+    void andreiSolntsevShouldBeFirstContributor(){
+
+        open("https://github.com/selenide/selenide");
+
+        $("div.Layout-sidebar").$(byText("Contributors"))
+                .closest(".BorderGrid-cell").$$("ul li").first().hover();
+        sleep(2000);
+        //$$(".Popover-message").shouldHave(text("Andrei Solntsev"));
+        $("[aria-label='User Hovercard']").$("a").shouldHave(text("Andrei Solntsev"));
+        sleep(5000);
+    }
+}
--- src/test/java/github/selenideRepoSearch.java
+++ src/test/java/github/selenideRepoSearch.java
@@ -1,8 +1,10 @@
 package github;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class selenideRepoSearch {
@@ -16,7 +18,7 @@ public class selenideRepoSearch {
         $("[data-target='qbsearch-input.inputButtonText']").click();
         $("#query-builder-test").setValue("selenide").pressEnter();
         $("[data-testid='results-list']").$("a").click();
-
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
         sleep(5000);
 
     }
